8db640f8dc5422ebf0fabeede044db22c30ba025
Nikodem Wójciak <nikodem1703@gmail.com>
2022-08-18 17:46:28 +0200

    initial commit

---
+++ src/test/java/com/example/kluczeautoinkrementacja/KluczeAutoinkrementacjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kluczeautoinkrementacja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KluczeAutoinkrementacjaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
